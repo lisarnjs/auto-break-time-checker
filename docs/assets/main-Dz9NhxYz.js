74bf7a9d98db8105d2b02ab16a17ac767577a1b4
lisarnjs <kwonenseo@gmail.com>
2025-03-01 16:14:54 +0900

    test: build

---
+++ docs/assets/main-Dz9NhxYz.js
@@ -0,0 +1,1 @@
+import"./404-C7Kq_qvh.js";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "auto-break-time-checker",
+  build: {
+    outDir: "docs", // dist -> docs 변경
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        // github pages는 SPA를 지원하지 않아 새로고침 시 404가 떠서, docs/404.html 파일 자동 생성하게끔 추가해줌.
+        404: "./index.html", // 404 페이지도 index.html로 처리
+      },
+    },
+  },
+});
