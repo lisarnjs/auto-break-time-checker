3a8b4f9758f6d22385bc9b641f301c3135e9ca5d
lisarnjs <kwonenseo@gmail.com>
2025-03-01 15:42:30 +0900

    feat: create automation time checker to check break time and study time

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ practice for...
 1. github pages deploy
 2. github actions workflows settings
 3. practice jest test code
-4. create automation timer for check rest time
+4. create automation timer for check break time
+
+| You can run with `npm run dev`
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [status, setStatus] = useState("🔥 STUDY TIME");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setCurrentTime(now);
+
+      const minutes = now.getMinutes();
+      if (minutes % 60 >= 50) {
+        setStatus("🍯 BREAK TIME");
+      } else {
+        setStatus("🔥 STUDY TIME");
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Automation Timer For Groom Player</h3>
+      <h1>{status}</h1>
+      <h2>{currentTime.toLocaleTimeString()}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
