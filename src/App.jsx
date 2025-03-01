de37cf70c94c94ffa4283464827e833a2a0e169b
lisarnjs <kwonenseo@gmail.com>
2025-03-01 20:12:28 +0900

    feat: 50분,정각에 종 울림 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+const BREAK_TIME_MINUTES = 50;
+const STUDY_TIME_MINUTES = 60;
+
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [status, setStatus] = useState("🔥 STUDY TIME");
   const dayToKoreanList = ["일", "월", "화", "수", "목", "금", "토"];
   const dayToKorean = dayToKoreanList[currentTime.getDay()];
+  const currentMinutes = currentTime.getMinutes();
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
@@ -26,6 +30,10 @@ function App() {
     <>
       <h3>Automation Timer</h3>
       <h2>오늘은 {dayToKorean}요일</h2>
+
+      {[BREAK_TIME_MINUTES, STUDY_TIME_MINUTES].includes(currentMinutes) && (
+        <strong className="break-time-bell">🔔</strong>
+      )}
       <h1>{status}</h1>
       <h2>{currentTime.toLocaleTimeString()}</h2>
     </>
